99d5bd1ee7dec071ade20fc8fed9512aa23bc42a
Jumpsquad <1thakkarpan@hdsb.ca>
2017-06-04 23:00:17 -0400

    Changes
    Score background code is up to date, just need to write the code for it

---
+++ CSFinal/src/core/Score.java
@@ -0,0 +1,19 @@
+package core;
+
+public class Score {
+
+	public int lives = 3;
+	public int scoreBase = 1000;
+	public int scoreLost0 = 500;
+	public int scoreLost1 = 400;
+	public int scoreLost2 = 300;
+	public int scoreLost3 = 200;
+	
+	public static void main(String[] args) {
+		
+	}
+
+	public static void updateLive(){
+		
+	}
+}
